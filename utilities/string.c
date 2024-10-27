e957547b7ad8b6c4e171149db5e028a56c10d604
Mițca Dumitru <dumitru0mitca@gmail.com>
2024-10-27 13:24:59 +0200

    utilities: Add some string functions based on K&R

---
+++ utilities/string.c
@@ -0,0 +1,25 @@
+#include "xil_types.h"
+
+// Adapted from "The C Programming language", 2nd ed., p 104, Kernighan and Ritchie
+int mystrcmp(u8 const* s, u8 const* t)
+{
+    for (; *s == *t; s++, t++) {
+        if (*s == '\0') {
+            return 0;
+        }
+    }
+
+    return *s - *t;
+}
+
+// Adapted from "The C Programming language", 2nd ed., p 103, Kernighan and Ritchie
+int mystrlen(u8 const* s)
+{
+    u8 const* p = s;
+
+    while (*p != '\0') {
+        p++;
+    }
+
+    return p - s;
+}
